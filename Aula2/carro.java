793c43f3553c8ab40f629918df7a23581736021e
ROGERIOBERTOLINO <rogeriolemos81@gmail.com>
2026-08-17 19:54:24 -0300

    Aula3 - Exemplo java sintaxe e entrada de dados

--- Aula2/carro.java
+++ Aula2/carro.java
@@ -2,14 +2,20 @@
 
 package Aula2; // package é por conta da pasta criada
 
-
+//criando construtur    
 class Carro {
 // Atributos
 // Cria uma variavel marca do tipo String
 String marca;
 String cor;
 
-// Metodo (Ação)
+// criando construtor
+public Carro(String marcaescolhida, String corescolhida){ 
+    this.marca = marcaescolhida;
+    this.cor = corescolhida;
+}
+
+//metodo (ação)
 
 void buzinar(){
     // Exibir mensagem no terminal
